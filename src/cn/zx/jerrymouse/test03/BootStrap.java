f66f0456d91190c9197c3acadcf411a71816afc2
waakaakaa <zju.zhangxin@gmail.com>
2013-02-04 12:45:00 +0800

    connector for jerrymouse of chapter3

---
+++ src/cn/zx/jerrymouse/test03/BootStrap.java
@@ -0,0 +1,8 @@
+package cn.zx.jerrymouse.test03;
+
+public class BootStrap {
+	public static void main(String[] args) {
+		HttpConnector connector = new HttpConnector();
+		connector.start();
+	}
+}
