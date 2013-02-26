6926d4d5c11683db7a1b8acde1bb4db6209671f5
waakaakaa <zju.zhangxin@gmail.com>
2013-02-26 21:29:41 +0800

    load all the source files

--- src/cn/zx/jerrymouse/test02/Constants.java
+++
@@ -1,9 +0,0 @@
-package cn.zx.jerrymouse.test02;
-
-import java.io.File;
-
-public class Constants {
-
-	public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
-
-}
--- src/cn/zx/jerrymouse/test03/BootStrap.java
+++
@@ -1,8 +0,0 @@
-package cn.zx.jerrymouse.test03;
-
-public class BootStrap {
-	public static void main(String[] args) {
-		HttpConnector connector = new HttpConnector();
-		connector.start();
-	}
-}
